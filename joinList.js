4a23d3b5af9b02fea4c8511a0f837c6e1f1d398d
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-10 16:41:38 -0700

    Create joinList.js

---
+++ joinList.js
@@ -0,0 +1,17 @@
+const joinList = function (conceptList) {
+  let addStrings = ''
+  for (let i = 0; i < conceptList.length; i++) {
+    const word = conceptList[i]
+
+    if (i < conceptList.length - 1) {
+      addStrings += word.toString() + ', '
+    } else {
+      addStrings += word.toString() + ''
+    }
+  }
+  return addStrings
+}
+
+const conceptList = ['gists', 'types', 'operators', 'iteration', 'problem solving']
+const concepts = joinList(conceptList)
+console.log(`Today I learned about ${concepts}.`)
