acd180f8088d8937b9c0aff1ba0506ba43ee6f8d
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-12 23:19:28 +0000

    "mapCallback"

---
+++ functions/map_builtin/mapTest.js
@@ -0,0 +1,9 @@
+const lighthouses = ['Gibraltar Point', "Peggy's Point", 'Cove Island', 'Discovery Island', 'Cape Scott', 'Point Clark', 'Kincardine']
+
+const size = lighthouses.map(element => element.length)
+console.log()
+console.log(size)
+
+
+// went through the array and counter letters for each entree
+// with the use of .map callback
