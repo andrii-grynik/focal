52eecb452159862e40840e7d830bd2aa6557da4f
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-12 03:42:44 +0000

    "Find Waldo"

---
+++ callbacks-intro/findWaldo.js
@@ -0,0 +1,14 @@
+// The second argument/parameter is expected to be a function
+function findWaldo (list, found) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === 'Waldo') {
+      found(i)
+    }
+  }
+}
+
+function actionWhenFound (i) {
+  console.log('Found Waldo at index ' + i + '!')
+}
+
+findWaldo(['Alice', 'Bob', 'Waldo', 'Winston'], actionWhenFound)
