22f4945798223ce4f1b4745e16576b3896c29036
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-15 23:29:31 +0000

    "Questions "

---
+++ W1MockTest/MOckTestQ01.js
@@ -0,0 +1,59 @@
+/* Question 1
+
+ *
+ *  Implement the functions defined below
+ *
+ */
+
+/* ===========================================================================
+ * MIN - the lowest value in a list
+ *
+ * For example:
+ *
+ *    min([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    0
+ */
+const min = function(arr) {
+  console.log(Math.min(...arr))
+}
+
+
+/* ===========================================================================
+ * MAX - the highest value in a list
+ *
+ * For example:
+ *
+ *    max([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    9
+ */
+const max = function(arr) {
+  console.log(Math.max(...arr))
+
+}
+
+/* ===========================================================================
+ * RANGE - the difference between the highest and lowest values in a list
+ *
+ * For example:
+ *
+ *    range([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    9
+ */
+const range = function(arr) {
+  const highest = Math.max(...arr)
+  const lowest =  Math.min(...arr)
+  console.log(highest - lowest)
+}
+
+// Don't change below:
+
+module.exports = { min, max, range };
