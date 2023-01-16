48d78ae43edf85643326f8d2b98d9bb790e4918a
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-16 14:16:33 -0700

    Update MOckTestQ01.js

--- W1MockTest/MOckTestQ01.js
+++ W1MockTest/MOckTestQ01.js
@@ -1,59 +1,79 @@
-/* Question 1
-
+/* Question 0
  *
  *  Implement the functions defined below
  *
  */
 
 /* ===========================================================================
- * MIN - the lowest value in a list
+ * COUNT - the number of items in a list
  *
  * For example:
  *
- *    min([6,2,3,4,9,6,1,0,5])
- *
- * Returns:
- *
- *    0
- */
-const min = function(arr) {
-  console.log(Math.min(...arr))
-}
-
-
-/* ===========================================================================
- * MAX - the highest value in a list
- *
- * For example:
- *
- *    max([6,2,3,4,9,6,1,0,5])
+ *    count([6,2,3,4,9,6,1,0,5]);
  *
  * Returns:
  *
  *    9
  */
-const max = function(arr) {
-  console.log(Math.max(...arr))
-
-}
+const count = function(arr) {
+  let total = 0;
+  for (i in arr) {
+    total++;
+  }
+  return (total);
+};
 
 /* ===========================================================================
- * RANGE - the difference between the highest and lowest values in a list
+ * SUM - the sum of the numbers in a list
+       - safe to assume that all items are numbers already
  *
  * For example:
  *
- *    range([6,2,3,4,9,6,1,0,5])
+ *    sum([6,2,3,4,9,6,1,0,5])
  *
  * Returns:
  *
- *    9
+ *    36
  */
-const range = function(arr) {
-  const highest = Math.max(...arr)
-  const lowest =  Math.min(...arr)
-  console.log(highest - lowest)
-}
+const sum = function(arr) {
+  let totalSum = 0;
+  for (const i in arr) {
+    totalSum += arr[i];
+  }
+  return (totalSum);
+};
+
+// To be used by mean. Do not alter.
+const round = function(number) {
+  return Math.round(number * 100) / 100;
+};
+
+/* ===========================================================================
+ * MEAN - the average value of numbers in a list
+ *      - use the provided 'round' function when returning your value
+ *      - if empty array, return null to indicate that mean cannot be calculated
+ *
+ * For example:
+ *
+ *    mean([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    4
+ */
+const mean = function(arr) {
+  let totalSum = 0;
+  if (!arr.length) {
+    return null;
+  }
+  for (const i in arr) {
+    totalSum += arr[i];
+  }
+  const numsCnt = arr.length;
+  const average = totalSum / numsCnt;
+
+  return round(average);
+};
 
 // Don't change below:
-
-module.exports = { min, max, range };
+module.exports = { count, sum, mean };
