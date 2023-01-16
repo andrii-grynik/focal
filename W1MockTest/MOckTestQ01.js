03e2f5e1eb398168ca1153a2947fad5ed24b1fc7
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-16 14:17:48 -0700

    Update MOckTestQ01.js

--- W1MockTest/MOckTestQ01.js
+++ W1MockTest/MOckTestQ01.js
@@ -1,79 +1,57 @@
-/* Question 0
+/* Question 1
  *
  *  Implement the functions defined below
  *
  */
 
 /* ===========================================================================
- * COUNT - the number of items in a list
+ * MIN - the lowest value in a list
  *
  * For example:
  *
- *    count([6,2,3,4,9,6,1,0,5]);
+ *    min([6,2,3,4,9,6,1,0,5])
+ *
+ * Returns:
+ *
+ *    0
+ */
+const min = function(arr) {
+  return (Math.min(...arr));
+};
+
+
+/* ===========================================================================
+ * MAX - the highest value in a list
+ *
+ * For example:
+ *
+ *    max([6,2,3,4,9,6,1,0,5])
  *
  * Returns:
  *
  *    9
  */
-const count = function(arr) {
-  let total = 0;
-  for (i in arr) {
-    total++;
-  }
-  return (total);
+const max = function(arr) {
+  return (Math.max(...arr));
 };
 
 /* ===========================================================================
- * SUM - the sum of the numbers in a list
-       - safe to assume that all items are numbers already
+ * RANGE - the difference between the highest and lowest values in a list
  *
  * For example:
  *
- *    sum([6,2,3,4,9,6,1,0,5])
+ *    range([6,2,3,4,9,6,1,0,5])
  *
  * Returns:
  *
- *    36
+ *    9
  */
-const sum = function(arr) {
-  let totalSum = 0;
-  for (const i in arr) {
-    totalSum += arr[i];
-  }
-  return (totalSum);
-};
-
-// To be used by mean. Do not alter.
-const round = function(number) {
-  return Math.round(number * 100) / 100;
-};
-
-/* ===========================================================================
- * MEAN - the average value of numbers in a list
- *      - use the provided 'round' function when returning your value
- *      - if empty array, return null to indicate that mean cannot be calculated
- *
- * For example:
- *
- *    mean([6,2,3,4,9,6,1,0,5])
- *
- * Returns:
- *
- *    4
- */
-const mean = function(arr) {
-  let totalSum = 0;
-  if (!arr.length) {
-    return null;
-  }
-  for (const i in arr) {
-    totalSum += arr[i];
-  }
-  const numsCnt = arr.length;
-  const average = totalSum / numsCnt;
-
-  return round(average);
+const range = function(arr) {
+  const highest = Math.max(...arr);
+  const lowest =  Math.min(...arr);
+  return (highest - lowest);
 };
 
 // Don't change below:
-module.exports = { count, sum, mean };
+
+module.exports = { min, max, range };
