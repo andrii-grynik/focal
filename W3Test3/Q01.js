03f83068475077989f4f412fe54bbabef1190480
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-29 09:45:10 -0700

    Create Q01.js

---
+++ W3Test3/Q01.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/* Question 01
+
+Convert an array of arrays into an object.
+
+This is the inverse of the previous question.
+
+Arrays will only have two elements: [key, value]
+
+To keep this simple, the values will only be primitive types (number, string, etc.) and not other objects or array.
+
+Furthermore, assume that the input is always clean/accurate. In other words, don't worry about handling edge cases.
+
+Examples:
+
+- arrayToObject([['a', 1], ['b', 2], ['c', 3]])
+  => { a: 1, b: 2, c: 3 }
+- arrayToObject([['name', 'Dave'], ['role', 'Instructor'], ['yearsOfExperience', 10]])
+  => {name: 'Dave', role: 'Instructor', yearsOfExperience: 10}
+*/
+
+const arrayToObject = function(arr) {
+  let rv = {};
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] !== undefined) {
+      rv[arr[i][0]] = arr[i][1];
+    }
+  } return rv;
+};
+
+
+// Don't change below:
+module.exports = { arrayToObject };
+
+//also can be solved witj one liner of object.forEach
