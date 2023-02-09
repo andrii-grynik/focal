98582116592d406c294775ce9776b611b5280a59
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-02-09 16:11:30 -0700

    Create Q04.js

---
+++ W4Test4/Q04.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/* Question 04
+
+Write a function which creates and returns a promise.
+
+Its job will be similar to that of Question 01:
+> Run a given (callback) function after a delay.
+
+However:
+- if the given callback returns a falsy value, the promise should fail (reject)
+  the string "Falsy value retrieved" should be sent through to the reject function
+- if the given callback returns a truthy value, the promise should pass (resolve)
+  the return value of the executed callback should be sent through to the resolve function
+
+*/
+const doShortlyExpectingTruthy = function(callback, delay, data) {
+setTimeout(() => {
+  
+  callback(data);
+  
+  }, delay);
+  
+};
+  
+  function callback(data) {
+  
+  (delay / 1000 + data);
+  
+};
+
+
+// Don't change below:
+module.exports = { doShortlyExpectingTruthy };
