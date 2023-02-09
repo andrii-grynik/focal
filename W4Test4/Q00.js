4c7458efa62d50b0ec9023596adf2ed82b07751b
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-02-09 16:04:04 -0700

    Create Q00.js

---
+++ W4Test4/Q00.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/* Question 00
+
+Given a collection of game outcome records, determine who all the players are by returning an array of their names.
+
+The array should be ordered by how the names are encountered.
+
+Example Input:
+
+[
+  { winner: 'Alishah', loser: 'Bob',    loser_points: 3 },
+  { winner: 'Maria',   loser: 'Xu Jin', loser_points: 1 },
+  { winner: 'Elise',   loser: 'Bob',    loser_points: 2 },
+  { winner: 'Elise',   loser: 'Maria',  loser_points: 4 },
+  { winner: 'Alishah', loser: 'Maria',  loser_points: 2 },
+  { winner: 'Maria',   loser: 'Xu Jin', loser_points: 3 },
+  { winner: 'Xu Jin',  loser: 'Elise',  loser_points: 2 }
+]
+
+Expected Result:
+
+['Alishah', 'Bob', 'Maria', 'Xu Jin', 'Elise']
+
+*/
+
+
+const allPlayerNames = function(outcomes) {
+  // IMPLEMENT ME
+  const players = [];
+  if (outcomes.length === 0) {
+    return players;
+  }
+  for (const element of outcomes) {
+    console.log(element.loser);
+    if (!players.includes(element.winner)) {
+      players.push(element.winner);
+    } if (!players.includes(element.loser)) {
+      players.push(element.loser);
+    }
+  }
+
+  return players;
+};
+
+
+// Don't change below:
+module.exports = { allPlayerNames };
